2a56f078fe59d54774f810d94b1ffe487b76cf11
ea-st <119245891+ea-st@users.noreply.github.com>
2023-01-11 09:06:22 +0000

    Made alacritty swallow

--- config.def.h
+++ config.def.h
@@ -33,7 +33,7 @@ static const Rule rules[] = {
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */ /* swallow patch */
 	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 }, /* swallow patch */
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 }, /* swallow patch */
-	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 }, /* swallow patch */
+	{ "Alacritty",      NULL,     NULL,           0,         0,          1,           0,        -1 }, /* swallow patch */
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */ /* swallow patch */
 };
 
